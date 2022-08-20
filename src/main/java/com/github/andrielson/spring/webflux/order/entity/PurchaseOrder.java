d41a002d17ef8730a30545e957be5c5cc4bc9083
Andrielson Silva <andrielson@gmail.com>
2022-08-19 22:02:23 -0300

    Add entity

---
+++ src/main/java/com/github/andrielson/spring/webflux/order/entity/PurchaseOrder.java
@@ -0,0 +1,23 @@
+package com.github.andrielson.spring.webflux.order.entity;
+
+import com.github.andrielson.spring.webflux.order.dto.OrderStatus;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@ToString
+public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String productId;
+    private Integer userId;
+    private Integer amount;
+    private OrderStatus status;
+}
